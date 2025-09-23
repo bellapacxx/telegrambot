51803b9115e8151f55e14c8dc9a8d25009c652d6
bellapacx <bellapacx@gmail.com>
2025-09-24 00:05:42 +0300

    fix2ddsrdmasgjfssddsssxssss

--- src/commands/deposit.ts
+++ src/commands/deposit.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const token = process.env.TELEGRAM_BOT_TOKEN;
+const token = process.env.BOT_TOKEN;
 if (!token) throw new Error("TELEGRAM_BOT_TOKEN not defined in .env");
 
 const bot = new TelegramBot(token, { polling: true });
