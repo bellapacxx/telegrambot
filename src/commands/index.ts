d8c92a24547c0278fa70e30506d386ae45188369
bellapacx <bellapacx@gmail.com>
2025-09-24 00:19:16 +0300

    fix2ddsrdmasgjfssddsssxsssssd

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,20 +1,20 @@
 import { Telegraf } from "telegraf";
 import startCommand from "./start";
 import balanceCommand from "./balance";
-import depositCommand from "./deposit";
+import {depositCommand} from "./deposit";
 import withdrawCommand from "./withdraw";
 import playCommand from "./play";
-
+import TelegramBot from "node-telegram-bot-api";
 export const registerCommands = (bot: Telegraf<any>) => {
   console.log("Registering commands...");
-
+  const bots = new TelegramBot(process.env.BOT_TOKEN!, { polling: true });
   startCommand(bot);
   console.log("✅ /start command registered");
 
   balanceCommand(bot);
   console.log("✅ /balance command registered");
 
-  depositCommand(bot);
+  depositCommand(bots);
   console.log("✅ /deposit command registered");
 
   withdrawCommand(bot);
