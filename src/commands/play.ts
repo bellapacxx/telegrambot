bb09a61cd07a7c944c44b1a43c20ebe6ab40335e
bellapacx <bellapacx@gmail.com>
2025-09-26 20:19:28 +0300

    secondsssssssaksfkfdhsazssssddsddssdfdfdfdfxfdfsddd

--- src/commands/play.ts
+++ src/commands/play.ts
@@ -87,30 +87,31 @@ const playCommand = (bot: TelegramBot) => {
         if (!user || user.balance < stake) {
           await bot.sendMessage(
             chatId,
-            `❌ Insufficient balance!\n💰 Your balance: ${user?.balance ?? 0} ETB\n\nPlease deposit before playing.`,
+            `❌ Insufficient balance!\n💰 Your balance: ${
+              user?.balance ?? 0
+            } ETB\n\nPlease deposit before playing.`,
             { reply_markup: mainMenuKeyboard() }
           );
           return bot.answerCallbackQuery(query.id);
         }
 
-       
-
         // Respond to Telegram
-        await bot.sendMessage(chatId, `🎮 You selected *${stake} ETB*.\n✅!`, {
-  parse_mode: "Markdown",
-  reply_markup: {
-    inline_keyboard: [
-      [
-        {
-          text: "Open App",
-          web_app: { url: `https://bot-frontend-urwm.vercel.app/stake/${stake}?user=${telegramId}` },
-        },
-      ],
-      [{ text: "⬅ Back", callback_data: "play" }],
-    ],
-  },
-});
-
+        await bot.sendMessage(chatId, `🎮 You selected *${stake} ETB*.\n!`, {
+          parse_mode: "Markdown",
+          reply_markup: {
+            inline_keyboard: [
+              [
+                {
+                  text: "ጨወታ ጀምር",
+                  web_app: {
+                    url: `https://bot-frontend-urwm.vercel.app/stake/${stake}?user=${telegramId}`,
+                  },
+                },
+              ],
+              [{ text: "⬅ Back", callback_data: "play" }],
+            ],
+          },
+        });
 
         return bot.answerCallbackQuery(query.id);
       }
