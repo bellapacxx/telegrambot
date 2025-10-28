6e32c7c7603821a8567df118bb0badceaf011be5
bellapacx <bellapacx@gmail.com>
2025-10-28 19:54:19 +0300

    first commitsssssssssddssssssssssssskk

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 const BOT_TOKEN = process.env.BOT_TOKEN!;
 const app = express();
 
